128a4a2567b86bd819aa04f76f6647e8404482d0
JoaooGabriel <caetanojoaogabriel@gmail.com>
2022-02-02 12:37:53 -0300

    feat: create middleware verify code number type and length

---
+++ src/shared/middlewares/verify-code-number-type-length.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { BadRequestError } from "../helpers/response/error.response";
+
+export function VerifyCodeNumberTypeAndLength(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const codeNumber = request.params.codeNumber;
+  const changeCodeNumberTypeToNumber = +codeNumber;
+
+  if (isNaN(changeCodeNumberTypeToNumber)) {
+    throw new BadRequestError(
+      "Código do boleto inválido, deve conter apenas numeral"
+    );
+  }
+
+  const splittedCodeNumber = codeNumber.split("");
+
+  if (splittedCodeNumber.length !== 46) {
+    if (splittedCodeNumber.length !== 44) {
+      throw new BadRequestError(
+      "Código do boleto inválido, tamanho diferente inválido"
+      );
+    }
+  }
+
+  next();
+}
