b8bb62adff9755afdb87896205183ea67666ce7b
JoaooGabriel <caetanojoaogabriel@gmail.com>
2022-02-02 17:03:58 -0300

    feat: create identification and segment-identification enum

---
+++ src/shared/enums/identification-value-reference.enums.ts
@@ -0,0 +1,6 @@
+export enum IdentificationValueOrReference {
+  _6 = "Valor a ser cobrado efetivamente em reais - módulo 10",
+  _7 = "Quantidade de moeda - módulo 10",
+  _8 = "Valor a ser cobrado efetivamente em reais - módulo 11",
+  _9 = "Quantidade de moeda - módulo 11",
+}
---
+++ src/shared/enums/segment-identification.enums.ts
@@ -0,0 +1,10 @@
+export enum SegmentIdentification {
+  _1 = "Prefeituras",
+  _2 = "Saneamento",
+  _3 = "Energia Elétrica e Gás",
+  _4 = "Telecomunicações",
+  _5 = "Órgãos Governamentais",
+  _6 = "Carnes e Assemelhados ou demais Empresas / Órgãos que serão identificadas através do CNPJ",
+  _7 = "Multas de trânsito",
+  _9 = "Uso exclusivo do banco",
+}
