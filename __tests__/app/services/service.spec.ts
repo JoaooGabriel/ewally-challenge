69f1e92e95e9e816068d03f5c73aa68ca8791cb2
JoaooGabriel <caetanojoaogabriel@gmail.com>
2022-02-04 17:32:26 -0300

    test: change structure tests

--- __tests__/app/services/service.spec.ts
+++ __tests__/app/services/service.spec.ts
@@ -10,90 +10,107 @@ describe("Services tests", () => {
     codeNumber = global.codeNumber.success;
   });
 
-  // it("should be get scopes code number with success", () => {
-  //   const response = service.validateCodeNumber(codeNumber);
+  describe("success", () => {
+    // it("should be get scopes code number with success", () => {
+    //   const response = service.validateCodeNumber(codeNumber);
 
-  //   expect(response).toBe({
-  //     status: 200,
-  //     amount: "",
-  //     expirationDate: "",
-  //     barCode: "",
-  //   });
-  // });
+    //   expect(response).toBe({
+    //     status: 200,
+    //     amount: "",
+    //     expirationDate: "",
+    //     barCode: "",
+    //   });
+    // });
 
-  // it("should be get scopes code number without amount with success", () => {
-  //   const response = service.validateCodeNumber(codeNumber);
+    // it("should be get scopes code number without amount with success", () => {
+    //   const response = service.validateCodeNumber(codeNumber);
 
-  //   expect(response).toBe({
-  //     status: 200,
-  //     expirationDate: "",
-  //     barCode: "",
-  //   });
-  // });
+    //   expect(response).toBe({
+    //     status: 200,
+    //     expirationDate: "",
+    //     barCode: "",
+    //   });
+    // });
 
-  // it("should be get scopes code number without without date with success", () => {
-  //   const response = service.validateCodeNumber(codeNumber);
+    // it("should be get scopes code number without without date with success", () => {
+    //   const response = service.validateCodeNumber(codeNumber);
 
-  //   expect(response).toBe({
-  //     status: 200,
-  //     amount: "",
-  //     barCode: "",
-  //   });
-  // });
+    //   expect(response).toBe({
+    //     status: 200,
+    //     amount: "",
+    //     barCode: "",
+    //   });
+    // });
 
-  it("should be not get scopes code number by product indentification scope invalid", () => {
-    codeNumber = global.codeNumber.productIndentificationInvalid;
-    function verifyFunction() {
-      service.validateCodeNumber(codeNumber);
-    }
-
-    expect(verifyFunction).toThrowError(
-      "Código do boleto fora do padrão, número de Identificação do Produto inválido"
-    );
+    describe("segment identification", () => {
+      // it("should be get scopes code number with segment identification one", () => {
+      //   codeNumber = global.codeNumber.segmentIdentification.one;
+      //   const response = service.validateCodeNumber(codeNumber);
+      //   expect(response).toBe({
+      //     status: 200,
+      //     amount: "",
+      //     expirationDate: "",
+      //     barCode: "",
+      //   });
+      // });
+    });
   });
 
-  it("should be not get scopes code number by segment identification scope invalid", () => {
-    codeNumber = global.codeNumber.segmentIdentificationInvalid;
-    function verifyFunction() {
-      service.validateCodeNumber(codeNumber);
-    }
+  describe("error", () => {
+    it("should be not get scopes code number by product indentification scope invalid", () => {
+      codeNumber = global.codeNumber.productIndentificationInvalid;
+      function verifyFunction() {
+        service.validateCodeNumber(codeNumber);
+      }
 
-    expect(verifyFunction).toThrowError(
-      "Código do boleto fora do padrão, número de Identificação de Segmento inválido"
-    );
-  });
+      expect(verifyFunction).toThrowError(
+        "Código do boleto fora do padrão, número de Identificação do Produto inválido"
+      );
+    });
 
-  it("should be not get scopes code number by identification value effective or reference invalid", () => {
-    codeNumber =
-      global.codeNumber.identificationValueEffectiveOrReferenceinvalid;
-    function verifyFunction() {
-      service.validateCodeNumber(codeNumber);
-    }
+    it("should be not get scopes code number by segment identification scope invalid", () => {
+      codeNumber = global.codeNumber.segmentIdentification.invalid;
+      function verifyFunction() {
+        service.validateCodeNumber(codeNumber);
+      }
 
-    expect(verifyFunction).toThrowError(
-      "Código do boleto fora do padrão, número de Identificador de Valor Efetivo ou Referência inválido"
-    );
-  });
+      expect(verifyFunction).toThrowError(
+        "Código do boleto fora do padrão, número de Identificação de Segmento inválido"
+      );
+    });
 
-  it("should be not get scopes code number by verify digit (module 10) invalid", () => {
-    codeNumber = global.codeNumber.verifyDigitModule10Invalid;
-    function verifyFunction() {
-      service.validateCodeNumber(codeNumber);
-    }
+    it("should be not get scopes code number by identification value effective or reference invalid", () => {
+      codeNumber =
+        global.codeNumber.identificationValueEffectiveOrReferenceinvalid;
+      function verifyFunction() {
+        service.validateCodeNumber(codeNumber);
+      }
 
-    expect(verifyFunction).toThrowError(
-      "Código do boleto fora do padrão, número do Dígito Verificador inválido"
-    );
-  });
+      expect(verifyFunction).toThrowError(
+        "Código do boleto fora do padrão, número de Identificador de Valor Efetivo ou Referência inválido"
+      );
+    });
 
-  it("should be not get scopes code number by verify digit (module 11) invalid", () => {
-    codeNumber = global.codeNumber.verifyDigitModule11Invalid;
-    function verifyFunction() {
-      service.validateCodeNumber(codeNumber);
-    }
+    it("should be not get scopes code number by verify digit (module 10) invalid", () => {
+      codeNumber = global.codeNumber.verifyDigitModule10Invalid;
+      function verifyFunction() {
+        service.validateCodeNumber(codeNumber);
+      }
 
-    expect(verifyFunction).toThrowError(
-      "Código do boleto fora do padrão, número do Dígito Verificador inválido"
-    );
+      expect(verifyFunction).toThrowError(
+        "Código do boleto fora do padrão, número do Dígito Verificador inválido"
+      );
+    });
+
+    it("should be not get scopes code number by verify digit (module 11) invalid", () => {
+      codeNumber = global.codeNumber.verifyDigitModule11Invalid;
+      function verifyFunction() {
+        service.validateCodeNumber(codeNumber);
+      }
+
+      expect(verifyFunction).toThrowError(
+        "Código do boleto fora do padrão, número do Dígito Verificador inválido"
+      );
+    });
   });
 });
