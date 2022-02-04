363ba912cbb075dd6b8583a6964cdad65017734b
JoaooGabriel <caetanojoaogabriel@gmail.com>
2022-02-04 17:23:36 -0300

    feat: add globals variables

---
+++ src/shared/data/global.data.ts
@@ -0,0 +1,18 @@
+export const global = {
+  codeNumber: {
+    success: "846100000005246100291102005460339004695895061080",
+    productIndentificationInvalid:
+      "146100000005246100291102005460339004695895061080",
+    segmentIdentificationInvalid:
+      "806100000005246100291102005460339004695895061080",
+    identificationValueEffectiveOrReferenceinvalid:
+      "840100000005246100291102005460339004695895061080",
+    verifyDigitModule10Invalid:
+      "846100000005246100291102005460339004695895061080",
+    verifyDigitModule11Invalid:
+      "848100000005246100291102005460339004695895061080",
+    textContent: "codeNumber123",
+    withOutValue: "",
+    withOutDate: "",
+  },
+};
