7ef7ae2f32832597cfc3ad6a1b2973deacb787d3
JoaooGabriel <caetanojoaogabriel@gmail.com>
2022-02-10 15:59:40 -0300

    test: add error test, internal server error and bad request error

--- __tests__/app/controllers/controller.spec.ts
+++ __tests__/app/controllers/controller.spec.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import { Controller } from "./../../../src/app/controllers/controller";
 import { global } from "./../../../src/shared/data/global.data";
+import { BadRequestError } from "./../../../src/shared/helpers/response/error.response";
+
+function internalServerError(message: string) {
+  throw new Error(message);
+}
+
+function badRequestError(message: string) {
+  throw new BadRequestError(message);
+}
 
 describe("Controllers tests", () => {
   let controller: Controller;
@@ -24,7 +33,7 @@ describe("Controllers tests", () => {
   });
 
   describe("success", () => {
-    it("should be return data code number of billet with sucess", async () => {
+    it("should be return code number data of billet with sucess", async () => {
       codeNumber = global.codeNumber.success.valid;
       request = {
         params: {
@@ -39,6 +48,34 @@ describe("Controllers tests", () => {
         amount: "504.82",
         expirationDate: "2023-02-03",
         barCode: codeNumber,
+      });
+    });
+  });
+
+  describe("error", () => {
+    it("should not be return code number data, return internal server error", async () => {
+      await controller.getBillet(request as Request, response as Response);
+
+      expect(response.status).toHaveBeenCalledWith(500);
+      expect(response.json).toHaveBeenCalledWith({
+        error:
+          "Erro no servidor - Error TypeError: Cannot read property 'codeNumber' of undefined",
+      });
+    });
+
+    it("should not be return code number data, return bad request error", async () => {
+      codeNumber = global.codeNumber.invalid.productIndentification;
+      request = {
+        params: {
+          codeNumber,
+        },
+      };
+      await controller.getBillet(request as Request, response as Response);
+
+      expect(response.status).toHaveBeenCalledWith(400);
+      expect(response.json).toHaveBeenCalledWith({
+        error:
+          "Código do boleto fora do padrão, número de Identificação do Produto inválido",
       });
     });
   });
